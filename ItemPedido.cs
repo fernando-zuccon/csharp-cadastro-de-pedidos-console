8d99267c0623d44135f28b14faa800558e88f4cf
Fernando Zuccon <fernandozuccon@gmail.com>
2020-07-24 14:55:48 -0300

    Criado as classes Pedido e ItemPedido

---
+++ ItemPedido.cs
@@ -0,0 +1,9 @@
+namespace csharp_cadastro_de_pedidos_console
+{
+    public class ItemPedido
+    {
+        public string descricao { get; set; }
+        public double valor_unitario { get; set; }
+        public int quantidade { get; set; }
+    }
+}
---
+++ Pedido.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace csharp_cadastro_de_pedidos_console
+{
+    public class Pedido
+    {
+        private List<ItemPedido> Itens { get; set; }
+
+    }
+}
